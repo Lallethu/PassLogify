1ca384dfa091ae30f3ae06f3de378cd0d22050a0
Lallethu <lallethu.68@gmail.com>
2024-02-18 00:52:49 +0100

    Create different screens to populate the dashboard drawer.

---
+++ src/screens/CreateEntryScreen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const CreateEntryScreen = () => {
+	return (
+		<View>
+			<Text>CreateEntryScreen</Text>
+		</View>
+	);
+};
+
+export default CreateEntryScreen;
