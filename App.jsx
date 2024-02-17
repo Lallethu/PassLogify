372fe81cff22f041ed4c26f16c3be25fd0cb9d44
Lallethu <lallethu.68@gmail.com>
2024-02-17 21:43:54 +0100

    Modify the app file to have the root navigator.

--- App.jsx
+++ App.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import RootStackNavigation from './src/navigation/RootStack';
 
 const App = () => {
 	return (
-		<View>
-			<Text>Hello, React Native!</Text>
-		</View>
+		<NavigationContainer>
+			<RootStackNavigation />
+		</NavigationContainer>
 	);
 };
 
