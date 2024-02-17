f68624855ebdfec8fd4cdd7c553a7196606e3242
Lallethu <lallethu.68@gmail.com>
2024-02-17 16:58:07 +0100

    Set up the prettier config file.

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   arrowParens: 'avoid',
   bracketSameLine: true,
-  bracketSpacing: false,
+  bracketSpacing: true,
   singleQuote: true,
   trailingComma: 'all',
+  useTabs: true,
+  tabWidth: 4,
 };
