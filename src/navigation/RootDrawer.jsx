e6dd276f41a0985330d88faab0b641b6b18cbd15
Lallethu <lallethu.68@gmail.com>
2024-02-19 00:36:41 +0100

    Format the code.

--- src/navigation/RootDrawer.jsx
+++ src/navigation/RootDrawer.jsx
@@ -7,22 +7,19 @@ const RootDrawer = createDrawerNavigator();
 
 const RootDrawerNavigation = () => {
 	return (
-			<RootDrawer.Navigator>
-				<RootDrawer.Group>
-					<RootDrawer.Screen
-						name="Dashboard"
-						component={DashboardScreen}
-					/>
-          <RootDrawer.Screen
-            name="About"
-            component={AboutScreen}
-          />
-          <RootDrawer.Screen
-            name="App Settings"
-            component={AppSettingsScreen}
-          />
-				</RootDrawer.Group>
-			</RootDrawer.Navigator>
+		<RootDrawer.Navigator>
+			<RootDrawer.Group>
+				<RootDrawer.Screen
+					name="Dashboard"
+					component={DashboardScreen}
+				/>
+				<RootDrawer.Screen name="About" component={AboutScreen} />
+				<RootDrawer.Screen
+					name="App Settings"
+					component={AppSettingsScreen}
+				/>
+			</RootDrawer.Group>
+		</RootDrawer.Navigator>
 	);
 };
 
