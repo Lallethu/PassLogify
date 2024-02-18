8a9e1af4b482b3b4c73f38c8d1773b77208b1c43
Lallethu <lallethu.68@gmail.com>
2024-02-19 00:37:05 +0100

    Add two screen to the stack.

--- src/navigation/RootStack.jsx
+++ src/navigation/RootStack.jsx
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LandingScreen from '../screens/LandingScreen';
 import RootDrawerNavigation from './RootDrawer';
+import CreateEntryScreen from '../screens/CreateEntryScreen';
+import ManageGroupsScreen from '../screens/ManageGroupsScreen';
 
 const RootStack = createNativeStackNavigator();
 
@@ -13,6 +15,8 @@ const RootStackNavigation = () => {
 			<RootStack.Group>
 				<RootStack.Screen name="Landing" component={LandingScreen} />
         <RootStack.Screen name="Dashboard" component={RootDrawerNavigation} />
+        <RootStack.Screen name="Create" component={CreateEntryScreen} />
+        <RootStack.Screen name="Manage" component={ManageGroupsScreen} />
 			</RootStack.Group>
 		</RootStack.Navigator>
 	);
