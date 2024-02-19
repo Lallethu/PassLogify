bc5f0282f16f55b36c23a5ec94d465470e460ee3
Lallethu <lallethu.68@gmail.com>
2024-02-19 01:14:08 +0100

    Remove unnecessary code.

--- src/screens/CreateEntryScreen.jsx
+++ src/screens/CreateEntryScreen.jsx
@@ -2,23 +2,18 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import FakeBottomTab from '../components/FakeBottomTab';
 import { STYLES } from '../constantes/styles';
-import { SUPPORTING_COLORS } from '../constantes/colors';
 import CreateEntryForm from '../components/CreateEntryForm';
-import { CustomButton } from '../components/CustomButton';
-import { useNavigation } from '@react-navigation/core';
 import { BackToDashboardHeader } from '../components/BackToDashboardHeader';
 
 const CreateEntryScreen = () => {
-  const navigation = useNavigation();
-
 	return (
 		<>
-      <BackToDashboardHeader />
+			<BackToDashboardHeader />
 			<View style={STYLES.container}>
 				<CreateEntryForm />
 			</View>
 
-			<FakeBottomTab route={"Create"} />
+			<FakeBottomTab route={'Create'} />
 		</>
 	);
 };
