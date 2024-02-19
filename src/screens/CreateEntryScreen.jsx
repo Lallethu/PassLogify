105805a64542ba2720ee27e9bc6f25f73cec644e
Lallethu <lallethu.68@gmail.com>
2024-02-19 01:13:18 +0100

    Create and implement the screen responsible for the creation of entries.

--- src/screens/CreateEntryScreen.jsx
+++ src/screens/CreateEntryScreen.jsx
@@ -1,11 +1,25 @@
 import { View, Text } from 'react-native';
 import React from 'react';
+import FakeBottomTab from '../components/FakeBottomTab';
+import { STYLES } from '../constantes/styles';
+import { SUPPORTING_COLORS } from '../constantes/colors';
+import CreateEntryForm from '../components/CreateEntryForm';
+import { CustomButton } from '../components/CustomButton';
+import { useNavigation } from '@react-navigation/core';
+import { BackToDashboardHeader } from '../components/BackToDashboardHeader';
 
 const CreateEntryScreen = () => {
+  const navigation = useNavigation();
+
 	return (
-		<View>
-			<Text>CreateEntryScreen</Text>
-		</View>
+		<>
+      <BackToDashboardHeader />
+			<View style={STYLES.container}>
+				<CreateEntryForm />
+			</View>
+
+			<FakeBottomTab route={"Create"} />
+		</>
 	);
 };
 
