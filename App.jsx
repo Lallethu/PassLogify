8754382adb0c03c403fff8d7072f6d6f342fca6c
Lallethu <lallethu.68@gmail.com>
2024-02-20 23:44:07 +0100

    Change the import statement.

--- App.jsx
+++ App.jsx
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import RootStackNavigation from './src/navigation/RootStack';
-import ThemeProvider from './src/test/ThemeProvider';
+import ThemeProvider from './src/services/ThemeProvider';
 
 const App = () => {
 	return (
