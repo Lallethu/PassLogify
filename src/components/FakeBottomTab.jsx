8743851a2ad8ad7b66f819093a74848de9119d50
Lallethu <lallethu.68@gmail.com>
2024-02-20 23:45:53 +0100

    Implement correctly the UI theme in the component.

--- src/components/FakeBottomTab.jsx
+++ src/components/FakeBottomTab.jsx
@@ -1,11 +1,14 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { View, TouchableOpacity, Text, StyleSheet, Image } from 'react-native';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import { BRAND_COLORS } from '../constantes/colors';
+import useTheme from '../hooks/useTheme';
+import useThemedStyles from '../hooks/useThemeStyles';
 
 const FakeBottomTab = ({ route }) => {
+	const theme = useTheme();
+	const style = useThemedStyles(styles);
 	const navigation = useNavigation();
-	const [currentRoute, setCurrentRoute] = React.useState(route);
+	const [currentRoute, setCurrentRoute] = useState(route);
 
 	const handleNavigate = route => {
 		setCurrentRoute(route);
@@ -19,78 +22,79 @@ const FakeBottomTab = ({ route }) => {
 	);
 
 	return (
-		<View style={styles.tabContainer}>
+		<View style={style.tabContainer}>
 			<TouchableOpacity
-				style={styles.tab}
+				style={style.tab}
 				onPress={() => handleNavigate('Manage')}>
 				<Image
-					style={styles.tab.img}
+					style={style.tab.img}
 					source={require('../assets/background.png')}
 				/>
 				<Text
 					style={
 						currentRoute === 'Manage'
-							? [styles.tabText, styles.tabTextActive]
-							: styles.tabText
+							? [style.tabText, style.tabTextActive]
+							: style.tabText
 					}>
 					Manage entry
 				</Text>
 			</TouchableOpacity>
 			<TouchableOpacity
-				style={styles.tab}
+				style={style.tab}
 				onPress={() => handleNavigate('Create')}>
 				<Image
-					style={styles.tab.img}
+					style={style.tab.img}
 					source={require('../assets/background.png')}
 				/>
 				<Text
 					style={[
 						currentRoute === 'Create'
-							? [styles.tabText, styles.tabTextActive]
-							: styles.tabText,
+							? [style.tabText, style.tabTextActive]
+							: style.tabText,
 					]}>
 					Create entry
 				</Text>
 			</TouchableOpacity>
-			<TouchableOpacity style={styles.tab}>
+			<TouchableOpacity style={style.tab}>
 				<Image
-					style={styles.tab.img}
+					style={style.tab.img}
 					source={require('../assets/background.png')}
 				/>
-				<Text style={styles.tabText}>SURPRISE</Text>
+				<Text style={style.tabText}>SURPRISE</Text>
 			</TouchableOpacity>
 		</View>
 	);
 };
 
 // might, in the future change the styles to a more themed approach
-const styles = StyleSheet.create({
-	tabContainer: {
-		flexDirection: 'row',
-		justifyContent: 'space-around',
-		alignItems: 'center',
-		backgroundColor: '#fff',
-		borderTopWidth: 1,
-		borderTopColor: '#ccc',
-		paddingBottom: 10,
-	},
-	tab: {
-		flex: 1,
-		alignItems: 'center',
-		paddingTop: 10,
-		img: {
-			width: 20,
-			height: 20,
+const styles = theme =>
+	StyleSheet.create({
+		tabContainer: {
+			flexDirection: 'row',
+			justifyContent: 'space-around',
+			alignItems: 'center',
+			backgroundColor: theme.colors.BACKGROUND,
+			borderTopWidth: 1,
+			borderTopColor: theme.colors.BORDER,
+			paddingBottom: 10,
 		},
-	},
-	tabText: {
-		fontSize: 16,
-		fontWeight: 'bold',
-		color: '#333',
-	},
-	tabTextActive: {
-		color: BRAND_COLORS.primary[700],
-	},
-});
+		tab: {
+			flex: 1,
+			alignItems: 'center',
+			paddingTop: 10,
+			img: {
+				width: 20,
+				height: 20,
+			},
+		},
+		tabText: {
+			fontSize: 16,
+			fontWeight: 'bold',
+			color: theme.colors.TEXT_SECONDARY,
+		},
+		tabTextActive: {
+			color: theme.colors.TEXT,
+		},
+	});
 
 export default FakeBottomTab;
