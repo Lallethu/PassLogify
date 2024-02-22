08fbb4385fa70284a08022fa911eb67b88f27bd4
Lallethu <lallethu.68@gmail.com>
2024-02-22 23:07:59 +0100

    Completly finish the features: delete one update one. (BUG FREE)

--- src/screens/ManageGroupsScreen.jsx
+++ src/screens/ManageGroupsScreen.jsx
@@ -1,95 +1,266 @@
-import React, { useCallback, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Alert } from 'react-native';
 import FakeBottomTab from '../components/FakeBottomTab';
 import { FakeHeader } from '../components/FakeHeader';
 import useTheme from '../hooks/useTheme';
 import useThemedStyles from '../hooks/useThemeStyles';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEntry } from '../hooks/useEntry';
 import { useFocusEffect } from '@react-navigation/native';
+import CustomPrompt from '../components/CustomPrompt';
 import {
 	GestureHandlerRootView,
 	ScrollView,
 } from 'react-native-gesture-handler';
+import ExpendableLoginCard from '../components/ExpendableLoginCard';
 
 const ManageGroupsScreen = () => {
 	const theme = useTheme();
 	const style = useThemedStyles(styles);
 
+	const { getData, deleteData, updateEntry } = useEntry();
+	const [listOfLogins, setListOfLogins] = useState([]);
+	const [isPromptVisible, setIsPromptVisible] = useState(false);
+	const [groupLabel, setGroupLabel] = useState('');
+
+	const onDeleteGroup = async groupLabel => {
+		Alert.alert(
+			'Delete group',
+			`Are you sure you want to delete the group ${groupLabel}?`,
+			[
+				{
+					text: 'Cancel',
+					onPress: () => console.log('Cancel Pressed'),
+					style: 'cancel',
+				},
+				{
+					text: 'Delete',
+					onPress: async () => {
+						await deleteData(groupLabel);
+						const data = await getData();
+						setListOfLogins(data);
+
+            if (isPromptVisible) {
+              setIsPromptVisible(false);
+            }
+						console.log('data', data);
+						console.log('logins', listOfLogins);
+					},
+				},
+			],
+			{ cancelable: false },
+		);
+	};
+
+	const openRenamePrompt = groupLabel => {
+		setGroupLabel(groupLabel);
+		setIsPromptVisible(true);
+	};
+
+	const handleClosePrompt = () => {
+		setIsPromptVisible(false);
+	};
+
+const [isRenaming, setIsRenaming] = useState(false);
+
+const handleRenameGroup = async newGroupLabel => {
+  await updateEntry(groupLabel, newGroupLabel);
+  setIsPromptVisible(false);
+  setIsRenaming(true);
+};
+
+useEffect(() => {
+  if (isRenaming) {
+    const fetchData = async () => {
+      const data = await getData();
+      setListOfLogins(data);
+      setIsRenaming(false);
+    };
+    fetchData();
+  }
+}, [isRenaming]);
+
+	useFocusEffect(
+		useCallback(() => {
+			const fetchGroups = async () => {
+				const data = await getData();
+				setListOfLogins(data);
+			};
+			fetchGroups();
+		}, []),
+	);
+
 	return (
-		<GestureHandlerRootView style={{ flex: 1 }}>
+		<GestureHandlerRootView
+			style={[{ flex: 1, backgroundColor: theme.colors.BACKGROUND }]}>
 			<FakeHeader />
 			<View style={[style.container]}>
-					<Text style={[style.title]}>Manage Groups</Text>
-					<Text style={[style.text, style.box]}>
-						Here you can manage your groups. You can delete a group
-						and all entries associated with it. You can also rename
-						a group.
-					</Text>
-					<View style={{ height: 20 }} />
+				<Text style={[style.title, { marginBottom: 15 }]}>
+					Manage Groups
+				</Text>
+				<Text style={[style.text, style.box]}>
+					Here you can manage your groups. You can delete a group and
+					all entries associated with it. You can also rename a group.
+					{'\n'}As well as reorganize the order of the groups.
+				</Text>
+				<View style={{ height: 20 }} />
 				<ScrollView style={{ width: '100%' }}>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-          <Text style={[style.title, style.text]}>Manage Groups</Text>
-        </ScrollView>
+					{listOfLogins.length > 0 ? (
+						listOfLogins.map((login, index) => (
+							<ExpendableLoginCard
+								key={index}
+								{...{ login, index, styleFromTheme: style }}>
+								<Text
+									style={[
+										style.text,
+										{
+											color: theme.colors.DANGER,
+											fontWeight: 'bold',
+										},
+									]}
+									onPress={() => {
+										onDeleteGroup(login.groupLabel);
+									}}>
+									Delete
+								</Text>
+								<Text
+									style={[
+										style.text,
+										{
+											color: theme.colors.WARNING,
+											fontWeight: 'bold',
+										},
+									]}
+									onPress={() => {
+										openRenamePrompt(login.groupLabel);
+									}}>
+									Rename
+								</Text>
+							</ExpendableLoginCard>
+						))
+					) : (
+						<Text>No logins yet :)</Text>
+					)}
+				</ScrollView>
+				<CustomPrompt
+					visible={isPromptVisible}
+					onClose={handleClosePrompt}
+					onSubmit={handleRenameGroup}
+          text="Enter a new group label"
+          placeholder="e.g. Social Media"
+				/>
 			</View>
-      <FakeBottomTab route={'Manage'} />
+			<FakeBottomTab route={'Manage'} />
 		</GestureHandlerRootView>
 	);
 };
 
 const styles = theme =>
 	StyleSheet.create({
+		body: {
+			flex: 1,
+			backgroundColor: theme.colors.BACKGROUND,
+			justifyContent: 'space-evenly',
+			alignItems: 'center',
+			padding: 20,
+		},
 		container: {
 			flex: 1,
 			justifyContent: 'center',
 			alignItems: 'center',
-			backgroundColor: theme.colors.BACKGROUND,
+		},
+		header: {
+			backgroundColor: theme.colors.PRIMARY,
+			padding: 20,
+			justifyContent: 'center',
+			alignItems: 'center',
 		},
 		card: {
-			padding: 20,
+			backgroundColor: theme.colors.CARD,
+			padding: 10,
 			borderRadius: 5,
 			margin: 10,
 		},
+		expendableCard: {
+			backgroundColor: theme.colors.CARD,
+			padding: 10,
+			borderRadius: 5,
+			margin: 10,
+			flexDirection: 'column',
+			justifyContent: 'flex-start',
+			alignItems: 'left',
+			width: '80%',
+		},
 		title: {
-      marginTop: 10,
-      padding: 20,
-			fontSize: 20,
+			color: theme.colors.TITLE,
+			fontSize: theme.typography.size.L,
+			letterSpacing: theme.typography.letterSpacing.M,
 			fontWeight: 'bold',
-			letterSpacing: 1,
-      color: theme.colors.TEXT,
+		},
+		shortSection: {
+			flexDirection: 'column',
+			justifyContent: 'flex-start',
+			alignItems: 'left',
+			width: '80%',
 		},
 		text: {
-			fontSize: 16,
-			letterSpacing: 1,
+			color: theme.colors.TEXT,
+			fontSize: theme.typography.size.M,
+			letterSpacing: theme.typography.letterSpacing.S,
+			textAlign: 'justify',
+		},
+		referralCode: {
+			color: theme.colors.TEXT_SECONDARY,
+			fontSize: theme.typography.size.S,
+			letterSpacing: theme.typography.letterSpacing.L,
+			fontWeight: 'bold',
+		},
+		button: {
+			padding: 10,
+			borderRadius: 5,
+			margin: 10,
+		},
+		separator: {
+			borderBottomColor: theme.colors.BORDER,
+			borderBottomWidth: 1,
+			width: '100%',
+			margin: 10,
+			opacity: 0.5,
+		},
+		spacer: {
+			margin: 10,
+			borderBottomWidth: 1,
+			borderColor: theme.colors.BORDER,
+		},
+		cardTitle: {
+			color: theme.colors.TITLE,
+			fontSize: theme.typography.size.L,
+			letterSpacing: theme.typography.letterSpacing.M,
+			fontWeight: 'bold',
+		},
+		spacerNoMargin: {
+			margin: 0,
+			borderBottomWidth: 1,
+			borderColor: theme.colors.BORDER,
 		},
 		spacerHorizontal: {
-			marginHorizontal: 20,
+			...styles.spacer,
+			marginVertical: 0,
+			marginHorizontal: 5,
+			borderBottomWidth: 1,
+			borderColor: theme.colors.BORDER,
 		},
-    box: {
-      padding: 20,
-      backgroundColor: theme.colors.BACKGROUND_TINT,
-      color: theme.colors.TEXT,
-    },
+		spacerVertical: {
+			...styles.spacer,
+			marginHorizontal: 0,
+			marginBottom: 15,
+			borderBottomWidth: 1,
+			borderColor: theme.colors.BORDER,
+		},
+		box: {
+			padding: 20,
+			backgroundColor: theme.colors.BACKGROUND_TINT,
+			color: theme.colors.TEXT,
+		},
 	});
 
 export default ManageGroupsScreen;
