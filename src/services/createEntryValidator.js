19881bcc3f4a7546c7a74de3806baaf4d4ce83bf
Lallethu <lallethu.68@gmail.com>
2024-02-22 23:04:34 +0100

    Refactor the update validator.

--- src/services/createEntryValidator.js
+++ src/services/createEntryValidator.js
@@ -62,55 +62,14 @@ export const createEntryValidator = async (entry, toaster) => {
 	return true;
 };
 
-export const updateEntryValidator = async (entry, oldData, toaster) => {
-  const emailRegex = /\S+@\S+\.\S+/;
-
-  if (!entry) {
-    toaster('Please fill in the form');
-    return false;
-  }
-
-  if (!entry.groupLabel) {
-    toaster('Group Label is required');
-    return false;
-  }
-
-  if (!entry.password) {
-    toaster('Password is required');
-    return false;
-  }
-
-  if (entry.email && !emailRegex.test(entry.email)) {
-    toaster('Email is not valid');
-    return false;
-  }
-
-  if (!entry.email && !entry.username) {
-    toaster('Username or Email, at least one is required');
-    return false;
-  }
-
-  if (entry.password.length < 8) {
-    toaster('Password must be at least 8 characters long');
-    return false;
-  }
-
-  if (entry.groupLabel.length < 3) {
-    toaster('Group Label must be at least 3 characters long');
-    return false;
-  }
-
-  if (entry.username && entry.username.length < 3) {
-    toaster('Username must be at least 3 characters long');
-    return false;
-  }
-
-  if (entry.groupLabel !== oldData.groupLabel) {
-    if (await doLoginAllreadyExist(entry)) {
-      toaster('This entry already exists');
-      return false;
-    }
-  }
-
-  return true;
-}
+export const updateEntryValidator = async (entry, toaster) => {
+	const keys = await AsyncStorage.getAllKeys();
+	const keysLowerCase = keys.map(key => key.trim().toLowerCase());
+	const groupLabelLowerCase = entry.groupLabel.trim().toLowerCase();
+	const groupLabelChanged = entry.groupLabel !== entry.oldGroupLabel;
+	if (groupLabelChanged && keysLowerCase.includes(groupLabelLowerCase)) {
+		toaster('This entry already exists');
+		return false;
+	}
+	return true;
+};
